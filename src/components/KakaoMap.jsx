630aba2094cfb51ec0c27c26d39e91e0c6fc3e2a
unknown <rha542@gmail.com>
2023-10-26 08:26:21 +0900

    kakao.maps undefined

--- src/components/KakaoMap.jsx
+++ src/components/KakaoMap.jsx
@@ -1,19 +1,41 @@
 import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 const { kakao } = window
 
-const KakaoMap = () => {
+const KakaoMap = ({walkLocationIds}) => {
   const [map, setMap] = useState(null)
+  const [center, setCenter] = useState(null)
+
+  console.log(walkLocationIds)
 
   // Draw map for the first time only using useEffect hook.
   useEffect(() => {
-    const container = document.getElementById('map')
-    const options = { 
-      center: new kakao.maps.LatLng(33.450701, 126.570667),
-      level: 3
-    }
-    const kakaoMap = new kakao.maps.Map(container, options)
-    setMap(kakaoMap)
+    axios.get(`http://localhost:8080/locations/${walkLocationIds[0]}`)
+    .then(res => {
+      setCenter(res.data)
+      return {lat: res.data.latitude, lon: res.data.longitude}
+    })
   }, [])
+
+  if (center === null) {
+    return (
+      <>Loading...</>
+    )
+  }
+
+  console.log(center)
+
+  const container = document.getElementById('map')
+  const options = { 
+    center: new kakao.maps.LatLng(center.lat, center.lon),
+    level: 3
+  }
+  const kakaoMap = new kakao.maps.Map(container, options)
+  setMap(kakaoMap)
+
+  if (map === null) {
+    <>Drawing maps...</>
+  }
 
   return (
     // The map shows here, in a frame of size decribed in style tag below.
--- src/components/Neighbors.jsx
+++ src/components/Neighbors.jsx
@@ -4,9 +4,7 @@ import { useState, useEffect } from 'react'
 import KakaoMap from './KakaoMap'
 
 const Dogs = () => {
-
   const [dogs, setDogs] = useState([])
-
   useEffect(() => {
     axios
     .get('http://localhost:8080/dogs')
@@ -15,7 +13,6 @@ const Dogs = () => {
       }
     )
   }, [])
-
     return (
       <>
         <h1>Dogs</h1>
@@ -26,11 +23,34 @@ const Dogs = () => {
     )
 }
 
-const Neighbors = () => {
+
+const Neighbors = (props) => {
+  const [location, setLocation] = useState(null)
+  const [locationURL, setLocationURL] = useState(null)
+
+
+
+  // useEffect(() => {
+  //   axios.get(`http://localhost:8080/locations/${props.userDog.walkLocationIds[0]}`)
+  //   .then(
+  //     res => {
+  //       console.log(res.data)
+  //       setLocation({lat: res.data.latitude, lon: res.data.longitude})
+  //     }
+  //   )
+  //   .then(
+  //     setLocationURL(`https://map.kakao.com/link/map/${location.lat},${location.lon}`)
+  //   )
+  // }, [])
+
+  // if (locationURL === null) {
+  //   return (
+  //     <>Loading...</>
+  //   )
+  // }
   return (
     <>
-      <KakaoMap />
-      <Dogs />
+      <KakaoMap walkLocationIds={props.userDog.walkLocationIds}/>
     </>
   )
 }

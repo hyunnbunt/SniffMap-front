453c9751d98a1db740ff82b0a7f33d863b424713
unknown <rha542@gmail.com>
2023-10-26 14:48:07 +0900

    map finally works! need to mark the point

--- src/components/KakaoMap.jsx
+++ src/components/KakaoMap.jsx
@@ -4,42 +4,46 @@ const { kakao } = window
 
 const KakaoMap = ({walkLocationIds}) => {
   const [map, setMap] = useState(null)
-  const [center, setCenter] = useState(null)
+  const [point, setPoint] = useState(null)
 
   console.log(walkLocationIds)
+  console.log(walkLocationIds[0])
 
-  // Draw map for the first time only using useEffect hook.
   useEffect(() => {
-    axios.get(`http://localhost:8080/locations/${walkLocationIds[0]}`)
-    .then(res => {
-      setCenter(res.data)
-      return {lat: res.data.latitude, lon: res.data.longitude}
-    })
+    axios
+      .get(`http://localhost:8080/locations/${walkLocationIds[0]}`)
+      .then(res => {
+        console.log(res.data)
+        setPoint({lat: res.data.latitude, lng: res.data.longitude})
+        return res.data
+      })
+      .then(res => {
+
+        const container = document.getElementById('map')
+        const options = { 
+          center: new kakao.maps.LatLng(res.latitude, res.longitude),
+          level: 3
+        }
+        const kakaoMap = new kakao.maps.Map(container, options)
+        setMap(kakaoMap)
+      })
+    
   }, [])
 
-  if (center === null) {
-    return (
-      <>Loading...</>
-    )
-  }
-
-  console.log(center)
-
-  const container = document.getElementById('map')
-  const options = { 
-    center: new kakao.maps.LatLng(center.lat, center.lon),
-    level: 3
-  }
-  const kakaoMap = new kakao.maps.Map(container, options)
-  setMap(kakaoMap)
-
-  if (map === null) {
+  if (point === null) {
     <>Drawing maps...</>
   }
 
+  console.log('hello')
+  console.log(map)
+
   return (
-    // The map shows here, in a frame of size decribed in style tag below.
+      // The map shows here, in a frame of size decribed in style tag below.
+  <>
+  
+
     <div id="map" style={{width:'500px', height:'400px'}}></div>
+  </>
   )
 }
 

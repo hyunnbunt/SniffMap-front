9e7903dc4156da6e88b3b8833f8ac7225a8b2550
unknown <rha542@gmail.com>
2023-10-29 15:24:58 +0900

    markers on the map, working fine, friends location : will adjust the scale of the map to see all the marker at once from the beginning

--- src/components/KakaoMap.jsx
+++ src/components/KakaoMap.jsx
@@ -3,16 +3,13 @@ import axios from 'axios'
 const { kakao } = window
 
 const KakaoMap = ({friendIds, userDog}) => {
-  const [map, setMap] = useState(null)
-  const [myLocation, setMyLocation] = useState(null)
-  const [friendLocations, setFriendLocations] = useState(null)
-  const mapRef = useRef(null)
+  const [kakaoMap, setKakaoMap] = useState(null)
 
   const baseURL = 'http://localhost:8080/'
 
   const getMyLatLng = async () => {
     const response = await axios.get(baseURL+`locations/${userDog.walkLocationIds[0]}`)
-    return {lat: response.data.latitude, lng: response.data.longitude}
+    return {id: response.data.id, lat: response.data.latitude, lng: response.data.longitude}
   }
 
   const getFriendDogsLatLng = async () => {
@@ -21,57 +18,48 @@ const KakaoMap = ({friendIds, userDog}) => {
       return axios.get(baseURL+`locations/${friendDog.data.walkLocationIds[0]}`)
     }))
     return friendDogLocations.map(friendDogLocation => {
-      return {lat: friendDogLocation.data.latitude, lng: friendDogLocation.data.longitude}
+      return {id: friendDogLocation.data.id, lat: friendDogLocation.data.latitude, lng: friendDogLocation.data.longitude}
     })
   }
 
-
-
-  const drawMap = async () => {
-    const my = await getMyLatLng()
+  const getMarkerPoisitons = async () => {
+    const my = [await getMyLatLng()]
     const friends = await getFriendDogsLatLng()
-    console.log(my)
-    console.log(friends)
-    const container = mapRef.current
-    // const container = document.getElementById('map')
-    const options = { 
-      center: new kakao.maps.LatLng(my.lat, my.lng),
-      level: 3
-    }
-    const kakaoMap = new kakao.maps.Map(container, options)
-
-    // const positions = friends.map(friendLocation => {
-    //   return {title: friendLocation.id, latlng: new kakao.maps.LatLng(friendLocation.lat, friendLocation.lng)}
-    // })
-    // const imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"
-    // for (var i = 0; i < positions.length; i ++) {
-    //   const imageSize = new kakao.maps.Size(24, 35)
-    //   const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize)
-    //   const marker = new kakao.maps.Marker({
-    //     map: map, // 마커를 표시할 지도
-    //     position: positions[i].latlng, // 마커를 표시할 위치
-    //     title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-    //     image : markerImage // 마커 이미지 
-    //   })
-
-    // }
-    marker.setMap(kakaoMap)
+    return [...my, ...friends]
   }
   
   useEffect(() => {
-     drawMap().then(res => console.log("drew the"))
+    getMarkerPoisitons().then(res => {
+      const demoLatLng = {demoLat : 37.4792683, demoLng : 127.0139129}
+      const container = document.getElementById('map')
+      const options = { 
+        center: new kakao.maps.LatLng(demoLatLng.demoLat, demoLatLng.demoLng),
+        level: 3
+      }
+      const kakaoMap = new kakao.maps.Map(container, options)
+      const positions = res.map(position => {
+        return {title: position.id, latlng: new kakao.maps.LatLng(position.lat, position.lng)}
+      })
+      console.log(positions)
+      const imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"
+      const imageSize = new kakao.maps.Size(24, 35)
+      const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize)
+      for (let i = 0; i < positions.length; i ++) {
+        const marker = new kakao.maps.Marker({
+          map: kakaoMap, // 마커를 표시할 지도
+          position: positions[i].latlng, // 마커를 표시할 위치
+          title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+          image : markerImage // 마커 이미지 
+        })
+        marker.setMap(kakaoMap)
+      }
+    })
   }, [])
 
-  if (myLocation === null || friendLocations === null) {
-    return (
-      <>Loading...</>
-    )
-  }
 
   return (
-    // The map shows here, in a frame of size decribed in style tag below.
     <>
-      <div ref={mapRef} style={{width:'500px', height:'400px'}}></div>
+      <div id="map" style={{width:'500px', height:'400px'}}></div>
     </>
   )
 }
